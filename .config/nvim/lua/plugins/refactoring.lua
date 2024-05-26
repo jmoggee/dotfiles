4db9ff62dda2bae62bf41eb894655e9ca9dee4a4
Jean Moggee <jean@mogg.ee>
2024-05-26 07:50:40 +0200

    Update keybidnings for refactoring

--- .config/nvim/lua/plugins/refactoring.lua
+++ .config/nvim/lua/plugins/refactoring.lua
@@ -12,11 +12,11 @@ return {
 
     vim.keymap.set("x", "<leader>rv", ":Refactor extract_var ")
 
-    vim.keymap.set({ "n", "x" }, "<leader>ri", ":Refactor inline_var")
+    vim.keymap.set({ "n", "x" }, "<leader>ri", ":Refactor inline_var ")
 
-    vim.keymap.set("n", "<leader>rI", ":Refactor inline_func")
+    vim.keymap.set("n", "<leader>rI", ":Refactor inline_func ")
 
-    vim.keymap.set("n", "<leader>rb", ":Refactor extract_block")
-    vim.keymap.set("n", "<leader>rbf", ":Refactor extract_block_to_file")
+    vim.keymap.set("n", "<leader>rb", ":Refactor extract_block ")
+    vim.keymap.set("n", "<leader>rbf", ":Refactor extract_block_to_file ")
   end,
 }
