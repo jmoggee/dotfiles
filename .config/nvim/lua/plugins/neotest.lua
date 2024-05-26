0c23075b5d351a47e5ac40520d21eba9e8ad0f6a
Jean Moggee <jean@mogg.ee>
2024-05-26 07:37:10 +0200

    Add neotest

---
+++ .config/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,42 @@
+return {
+	"nvim-neotest/neotest",
+	dependencies = {
+		"nvim-neotest/nvim-nio",
+		"nvim-lua/plenary.nvim",
+		"antoinemadec/FixCursorHold.nvim",
+		"nvim-treesitter/nvim-treesitter",
+		-- Test runners
+		"nvim-neotest/neotest-python",
+	},
+
+	config = function()
+		local neotest = require("neotest")
+
+		neotest.setup({
+			adapters = {
+				require("neotest-python")({
+					dap = { justMyCode = false },
+				}),
+			},
+		})
+
+		vim.keymap.set("n", "<leader>tt", ":lua require('neotest').run.run({ suite = true })<cr>", { silent = true })
+		vim.keymap.set("n", "<leader>tn", ":lua require('neotest').run.run()<cr>", { silent = true })
+		vim.keymap.set(
+			"n",
+			"<leader>td",
+			":lua require('neotest').run.run({ strategy = 'dap' })<cr>",
+			{ silent = true }
+		)
+		vim.keymap.set("n", "<leader>tf", ":lua require('neotest').run.run(vim.fn.expand('%'))<cr>", { silent = true })
+		vim.keymap.set("n", "<leader>tk", ":lua require('neotest').run.stop()<cr>", { silent = true })
+		vim.keymap.set("n", "<leader>ts", ":lua require('neotest').summary.toggle()<cr>", { silent = true })
+		vim.keymap.set(
+			"n",
+			"<leader>to",
+			":lua require('neotest').output.open({ enter = true })<cr>",
+			{ silent = true }
+		)
+		vim.keymap.set("n", "<leader>tp", ":lua require('neotest').output_panel.toggle()<cr>", { silent = true })
+	end,
+}
