1971122d760c3cc9fcef13d74462c4d34f5a50f2
Jean Moggee <jean@mogg.ee>
2024-10-18 07:03:52 +0200

    Disable pyright hints which is causing duplicates with ruff

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,86 +1,90 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-      require("mason").setup()
-    end,
-  },
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
 
-  {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    config = function()
-      require("mason-lspconfig").setup({
-        auto_install = true,
-      })
-    end,
-  },
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		config = function()
+			require("mason-lspconfig").setup({
+				auto_install = true,
+			})
+		end,
+	},
 
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      local lspconfig = require("lspconfig")
+			local lspconfig = require("lspconfig")
 
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.tsserver.setup({
-        capabilities = capabilities,
-      })
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.tsserver.setup({
+				capabilities = capabilities,
+			})
 
-      lspconfig.basedpyright.setup({
-        capabilities = capabilities,
-        settings = {
-          basedpyright = {
-            disableOrganizeImports = true,
-          },
-          python = {
-            analysis = {
-              ignore = { "*" },
-            },
-          },
-        },
-      })
+			lspconfig.basedpyright.setup({
+				capabilities = (function()
+					local updatedCapabilities = vim.lsp.protocol.make_client_capabilities()
+					updatedCapabilities.textDocument.publishDiagnostics.tagSupport.valueSet = { 2 }
+					return updatedCapabilities
+				end)(),
+				settings = {
+					basedpyright = {
+						disableOrganizeImports = true,
+					},
+					python = {
+						analysis = {
+							ignore = { "*" },
+						},
+					},
+				},
+			})
 
-      lspconfig.ruff_lsp.setup({
-        capabilities = capabilities,
-        on_attach = function(client, bufnr)
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.code_action({
-                context = { only = { "source.organizeImports" } },
-                apply = true,
-              })
-              vim.wait(100)
-            end,
-          })
-        end,
-      })
+			lspconfig.ruff_lsp.setup({
+				capabilities = capabilities,
+				on_attach = function(_client, bufnr)
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.code_action({
+								context = { only = { "source.organizeImports" } },
+								apply = true,
+							})
+							vim.wait(100)
+						end,
+					})
+				end,
+			})
 
-      lspconfig.csharp_ls.setup({
-        capabilities = capabilities,
-      })
+			lspconfig.csharp_ls.setup({
+				capabilities = capabilities,
+			})
 
-      vim.keymap.set("n", "<leader>ch", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, {})
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-      vim.keymap.set("n", "<leader>rr", vim.lsp.buf.rename, {})
+			vim.keymap.set("n", "<leader>ch", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "<leader>rr", vim.lsp.buf.rename, {})
 
-      -- Format on save
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        pattern = "*",
-        callback = function()
-          vim.lsp.buf.format()
-        end,
-      })
-    end,
-  },
+			-- Format on save
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				pattern = "*",
+				callback = function()
+					vim.lsp.buf.format()
+				end,
+			})
+		end,
+	},
 }
