bde3a238a7fdce5cb6afeefbf49e5c3e64f10425
Jean Moggee <jean@mogg.ee>
2024-10-22 19:14:28 +0200

    Updated nvim lsp and tests

--- .config/nvim/lua/plugins/neotest.lua
+++ .config/nvim/lua/plugins/neotest.lua
@@ -1,54 +1,54 @@
 return {
-	"nvim-neotest/neotest",
-	dependencies = {
-		"nvim-neotest/nvim-nio",
-		"nvim-lua/plenary.nvim",
-		"antoinemadec/FixCursorHold.nvim",
-		"nvim-treesitter/nvim-treesitter",
-		-- Test runners
-		"nvim-neotest/neotest-python",
-	},
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-neotest/nvim-nio",
+    "nvim-lua/plenary.nvim",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    -- Test runners
+    "nvim-neotest/neotest-python",
+  },
 
-	config = function()
-		local neotest = require("neotest")
+  config = function()
+    local neotest = require("neotest")
 
-		neotest.setup({
-			adapters = {
-				require("neotest-python")({
-					dap = { justMyCode = false },
-				}),
-			},
-		})
+    neotest.setup({
+      adapters = {
+        require("neotest-python")({
+          dap = { justMyCode = false },
+        }),
+      },
+    })
 
-		vim.keymap.set("n", "<leader>tt", function()
-			require("neotest").run.run({ suite = true })
-		end)
-		vim.keymap.set("n", "<leader>tn", function()
-			require("neotest").run.run()
-		end)
-		vim.keymap.set("n", "<leader>td", function()
-			require("neotest").run.run({ strategy = "dap" })
-		end)
-		vim.keymap.set("n", "<leader>tf", function()
-			require("neotest").run.run(vim.fn.expand("%"))
-		end, { silent = true })
-		vim.keymap.set("n", "<leader>tk", function()
-			require("neotest").run.stop()
-		end)
-		vim.keymap.set("n", "<leader>ts", function()
-			require("neotest").summary.toggle()
-		end, { silent = true })
-		vim.keymap.set("n", "<leader>to", function()
-			require("neotest").output.open({ enter = true })
-		end)
-		vim.keymap.set("n", "<leader>tp", function()
-			require("neotest").output_panel.toggle()
-		end)
-		vim.keymap.set("n", "<leader>t,", function()
-			require("neotest").jump.prev({ status = "failed" })
-		end)
-		vim.keymap.set("n", "<leader>t;", function()
-			require("neotest").jump.next({ status = "failed" })
-		end)
-	end,
+    vim.keymap.set("n", "<leader>tt", function()
+      require("neotest").run.run({ suite = true })
+    end)
+    vim.keymap.set("n", "<leader>tn", function()
+      require("neotest").run.run()
+    end)
+    vim.keymap.set("n", "<leader>td", function()
+      require("neotest").run.run({ strategy = "dap" })
+    end)
+    vim.keymap.set("n", "<leader>tf", function()
+      require("neotest").run.run(vim.fn.expand("%"))
+    end, { silent = true })
+    vim.keymap.set("n", "<leader>tk", function()
+      require("neotest").run.stop()
+    end)
+    vim.keymap.set("n", "<leader>ts", function()
+      require("neotest").summary.toggle()
+    end, { silent = true })
+    vim.keymap.set("n", "<leader>to", function()
+      require("neotest").output.open({ enter = true })
+    end)
+    vim.keymap.set("n", "<leader>tp", function()
+      require("neotest").output_panel.toggle()
+    end)
+    vim.keymap.set("n", "<leader>t,", function()
+      require("neotest").jump.prev({ status = "failed" })
+    end)
+    vim.keymap.set("n", "<leader>t;", function()
+      require("neotest").jump.next({ status = "failed" })
+    end)
+  end,
 }
