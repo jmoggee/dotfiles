644ba0ae2345ec1cc932fe2d20fa304108b59d37
Jean Moggee <jean@mogg.ee>
2024-10-25 08:00:07 +0200

    Launch DAP in a new vim tab with current buffer to prevent it messing with existing splits

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -8,21 +8,85 @@ return {
   config = function()
     local dap, dapui = require("dap"), require("dapui")
 
+    -- Store previous tab/window
+    local previous_tab = nil
+    local previous_win = nil
+    local debug_tab = nil
+    local is_debug_open = false
+
+    local function _store_current_position()
+      previous_tab = vim.api.nvim_get_current_tabpage()
+      previous_win = vim.api.nvim_get_current_win()
+    end
+
+    local function _create_debug_tab()
+      vim.cmd('tabnew %')
+      debug_tab = vim.api.nvim_get_current_tabpage()
+    end
+
+    local function _restore_previous_position()
+      if previous_tab and vim.api.nvim_tabpage_is_valid(previous_tab) then
+        vim.api.nvim_set_current_tabpage(previous_tab)
+        if previous_win and vim.api.nvim_win_is_valid(previous_win) then
+          vim.api.nvim_set_current_win(previous_win)
+        end
+      end
+    end
+
+    local function _close_debug_tab()
+      if debug_tab and vim.api.nvim_tabpage_is_valid(debug_tab) then
+        vim.cmd('silent! ' .. vim.api.nvim_tabpage_get_number(debug_tab) .. 'tabclose')
+      end
+    end
+
+    local function _reset_state()
+      debug_tab = nil
+      previous_tab = nil
+      previous_win = nil
+      is_debug_open = false
+    end
+
+    -- Public functions
+    local function open_dap_in_tab()
+      if is_debug_open then
+        return
+      end
+
+      _store_current_position()
+      _create_debug_tab()
+      dapui.open()
+      is_debug_open = true
+    end
+
+    local function cleanup_dap_tab()
+      if not is_debug_open then
+        return
+      end
+
+      dapui.close()
+      _restore_previous_position()
+      _close_debug_tab()
+      _reset_state()
+    end
+
     dapui.setup({})
 
     require("dap-python").setup("~/.virtualenvs/debugpy/bin/python")
 
     dap.listeners.before.attach.dapui_config = function()
-      dapui.open()
+      open_dap_in_tab()
     end
     dap.listeners.before.launch.dapui_config = function()
-      dapui.open()
+      open_dap_in_tab()
     end
     dap.listeners.before.event_terminated.dapui_config = function()
-      dapui.close()
+      cleanup_dap_tab()
     end
     dap.listeners.before.event_exited.dapui_config = function()
-      dapui.close()
+      cleanup_dap_tab()
+    end
+    dap.listeners.before.disconnect.dapui_config = function()
+      cleanup_dap_tab()
     end
 
     require("dap.ext.vscode").load_launchjs()
