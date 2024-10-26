b7c15e1960b384dbc84339bcd243bf8183a8a489
Jean Moggee <jean@mogg.ee>
2024-10-26 07:43:41 +0200

    Updated nvim alpha start page

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -5,7 +5,7 @@ return {
 
   config = function()
     local alpha = require("alpha")
-    local dashboard = require("alpha.themes.startify")
+    local dashboard = require("alpha.themes.dashboard")
 
     dashboard.section.header.val = {
       [[                                                                       ]],
@@ -24,6 +24,20 @@ return {
       [[                                                                       ]],
       [[                                                                       ]],
     }
+
+    dashboard.section.buttons.val = {
+      dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("r", "  Recently used files", ":Telescope oldfiles<CR>"),
+      dashboard.button("f", "󰱼  Find file", ":Telescope find_files<CR>"),
+      dashboard.button("t", "󱎸  Find text", ":Telescope live_grep <CR>"),
+      dashboard.button("s", "  Settings", ":e $HOME/.config/nvim/init.lua | :cd %:p:h | split . | wincmd k | pwd<CR>"),
+      dashboard.button("q", "󰈆  Quit NVIM", ":qa<CR>"),
+    }
+
+    local handle = io.popen("fortune")
+    local fortune = handle:read("*a")
+    handle:close()
+    dashboard.section.footer.val = fortune
 
     alpha.setup(dashboard.opts)
   end,
