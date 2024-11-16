a2fdc2617f338f4823d5ac89fb34da7777567d35
Jean Moggee <jean@mogg.ee>
2024-11-16 05:49:54 +0200

    Assign some hotkeys for moving between and renaming tabs in neovim quickly

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -7,5 +7,10 @@ local keymap = vim.keymap.set
 keymap("i", "jk", "<esc>")
 keymap("t", "jk", "<c-\\><c-n>")
 
+-- Tabs
+keymap("n", "<Tab>", ":tabnext<CR>", { desc = "Next tab" })
+keymap("n", "<S-Tab>", ":tabprevious<CR>", { desc = "Previous tab" })
+keymap("n", "<leader><Tab>r", ":Tabby rename_tab ", { desc = "Rename tab" })
+
 -- Oil
 keymap("n", "-", "<cmd>Oil<cr>", { desc = "Open Oil" })
