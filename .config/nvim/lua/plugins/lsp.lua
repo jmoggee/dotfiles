c0bda4b5bb3aa9aa8af028d7451edde7820d5be0
Jean Moggee <jean@mogg.ee>
2025-07-20 06:00:01 +0200

    Add quick bindings for lsp diagnostics to neovim

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -45,9 +45,26 @@ return {
     config = function()
       discover_lsps()
 
-      vim.keymap.set("n", "gl", function()
-        vim.diagnostic.open_float()
-      end)
+      -- Keymaps for diagnostics
+      vim.api.nvim_set_keymap(
+        "n",
+        "<leader>do",
+        "<cmd>lua vim.diagnostic.open_float()<CR>",
+        { noremap = true, silent = true }
+      )
+      vim.api.nvim_set_keymap(
+        "n",
+        "<leader>dp",
+        "<cmd>lua vim.diagnostic.goto_prev()<CR>",
+        { noremap = true, silent = true }
+      )
+      vim.api.nvim_set_keymap(
+        "n",
+        "<leader>dn",
+        "<cmd>lua vim.diagnostic.goto_next()<CR>",
+        { noremap = true, silent = true }
+      )
+      vim.api.nvim_set_keymap("n", "<leader>dd", "<cmd>Telescope diagnostics<CR>", { noremap = true, silent = true })
     end,
   },
 }
