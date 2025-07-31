a5e6cfa18a7943773b6ab0cb4ceb293ec595399f
Jean Moggee <jean@mogg.ee>
2025-07-31 05:16:35 +0200

    Update hyprland special auto start with discord and jq filtering

--- .config/hypr/scripts/start_special.sh
+++ .config/hypr/scripts/start_special.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 APP_CMD="$1"
-APP_CLASS="$2"
-WORKSPACE="$3"
+WORKSPACE="$2"
+FILTER="$3"
 
 eval "$APP_CMD" &
 
 # Wait for the app's window to appear
-while ! hyprctl clients -j | grep -q "\"class\": \"$APP_CLASS\""; do
-    sleep 0.1
+while ! hyprctl clients -j | jq -e "map(select($FILTER)) | length > 0" >/dev/null; do
+  sleep 0.1
 done
 
-hyprctl dispatch movetoworkspace "special:$WORKSPACE"
 hyprctl dispatch togglespecialworkspace "$WORKSPACE"
