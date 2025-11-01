4f80bcfef3d43be874ee65b58a54bbbc513aa275
Jean Moggee <jean@mogg.ee>
2025-11-01 08:33:53 +0200

    Add dadbod to neovim and formatting

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,69 +1,74 @@
 return {
-	"saghen/blink.cmp",
+  "saghen/blink.cmp",
 
-	lazy = false,
+  lazy = false,
 
-	dependencies = {
-		"L3MON4D3/LuaSnip",
-		version = "v2.*",
-	},
+  dependencies = {
+    "L3MON4D3/LuaSnip",
+    version = "v2.*",
+  },
 
-	version = "v0.*",
-	opts = {
-		keymap = { preset = "default" },
+  version = "v0.*",
+  opts = {
+    keymap = { preset = "default" },
 
-		appearance = {
-			use_nvim_cmp_as_default = true,
-			nerd_font_variant = "mono",
-		},
+    appearance = {
+      use_nvim_cmp_as_default = true,
+      nerd_font_variant = "mono",
+    },
 
-		snippets = {
-			preset = "luasnip",
-			expand = function(snippet)
-				require("luasnip").lsp_expand(snippet)
-			end,
-			active = function(filter)
-				if filter and filter.direction then
-					return require("luasnip").jumpable(filter.direction)
-				end
-				return require("luasnip").in_snippet()
-			end,
-			jump = function(direction)
-				require("luasnip").jump(direction)
-			end,
-		},
+    snippets = {
+      preset = "luasnip",
+      expand = function(snippet)
+        require("luasnip").lsp_expand(snippet)
+      end,
+      active = function(filter)
+        if filter and filter.direction then
+          return require("luasnip").jumpable(filter.direction)
+        end
+        return require("luasnip").in_snippet()
+      end,
+      jump = function(direction)
+        require("luasnip").jump(direction)
+      end,
+    },
 
-		sources = {
-			default = { "lsp", "path", "snippets", "buffer" },
-			providers = {},
-		},
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer" },
+      per_filetype = {
+        sql = { "snippets", "dadbod", "buffer" },
+      },
+      providers = {
+        dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
+      },
+    },
 
-		completion = {
-			accept = {
-				auto_brackets = {
-					enabled = true,
-				},
-			},
+    completion = {
+      accept = {
+        auto_brackets = {
+          enabled = true,
+        },
+      },
 
-			trigger = {
-				show_on_insert_on_trigger_character = false,
-			},
+      trigger = {
+        show_on_insert_on_trigger_character = false,
+      },
 
-			documentation = {
-				auto_show = true,
-				auto_show_delay_ms = 200,
-			},
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 200,
+      },
 
-			menu = {
-				draw = {
-					treesitter = { "lsp" },
-				},
-			},
-		},
+      menu = {
+        draw = {
+          treesitter = { "lsp" },
+        },
+      },
+    },
 
-		signature = {
-			enabled = true,
-		},
-	},
-	opts_extend = { "sources.default" },
+    signature = {
+      enabled = true,
+    },
+  },
+  opts_extend = { "sources.default" },
 }
