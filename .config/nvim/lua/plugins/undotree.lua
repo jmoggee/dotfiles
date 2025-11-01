badecf3b1ac024bbd48d04705436e4fabd1bbe5b
Jean Moggee <jean@mogg.ee>
2025-11-01 05:29:29 +0200

    Add undotree to neovim

---
+++ .config/nvim/lua/plugins/undotree.lua
@@ -0,0 +1,9 @@
+return {
+  "mbbill/undotree",
+  cmd = "UndotreeToggle",
+  keys = {
+    { "<leader>uu", "<cmd>UndotreeToggle<cr>", desc = "Toggle Undotree" },
+    { "<leader>ur", "<cmd>UndotreeRefresh<cr>", desc = "Refresh Undotree" },
+    { "<leader>ul", "<cmd>UndotreeFocus<cr>", desc = "Focus Undotree" },
+  },
+}
