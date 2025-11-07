868f583bd31a44e3c2d3ba93cecbb6c1d2a3a9b3
Jean Moggee <jean@mogg.ee>
2025-11-07 15:50:02 +0200

    Neovim config updates

--- .config/nvim/lua/config/autocmd.lua
+++ .config/nvim/lua/config/autocmd.lua
@@ -18,3 +18,19 @@ vim.api.nvim_create_autocmd("TermOpen", {
     vim.opt_local.winhighlight = "Normal:NormalFloat,NormalNC:NormalFloat"
   end,
 })
+
+-- Add highlight for yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
+  pattern = "*",
+  desc = "highlight selection on yank",
+  callback = function()
+    vim.highlight.on_yank({ timeout = 200, visual = true })
+  end,
+})
+
+-- Split help vertical
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "help",
+  command = "wincmd L",
+})
