33570f64aea7cc4f600266027546a97e20910e46
Jean Moggee <jean@mogg.ee>
2025-11-12 06:39:59 +0200

    Clean colorschems in nvim

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,26 +1,5 @@
 return {
   {
-    "vague-theme/vague.nvim",
-    priority = 1000,
-    config = function()
-      -- vim.cmd.colorscheme("vague")
-
-      -- Only set custom statusline highlights if using vague colorscheme
-      if vim.g.colors_name == "vague" then
-        local colors = require("vague.config.internal").current.colors
-
-        vim.api.nvim_set_hl(0, "MiniStatuslineFilename", {
-          fg = colors.fg,
-          bg = colors.inactiveBg,
-        })
-        vim.api.nvim_set_hl(0, "MiniStatuslineInactive", {
-          fg = colors.comment,
-          bg = colors.inactiveBg,
-        })
-      end
-    end,
-  },
-  {
     "folke/tokyonight.nvim",
     priority = 1000,
     config = function()
@@ -34,57 +13,5 @@ return {
       })
       vim.cmd.colorscheme("tokyonight-night")
     end,
-  },
-  {
-    "rebelot/kanagawa.nvim",
-    priority = 1000,
-  },
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function(_, opts)
-      require("catppuccin").setup(opts)
-      -- vim.cmd.colorscheme("catppuccin")
-    end,
-  },
-  {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    priority = 1000,
-  },
-  {
-    "neanias/everforest-nvim",
-    priority = 1000,
-  },
-  {
-    "webhooked/kanso.nvim",
-    priority = 1000,
-    opts = {
-      overrides = function(colors)
-        local theme = colors.theme
-        return {
-          WinSeparator = { fg = colors.palette.inkBg0 },
-          StatusLine = { fg = theme.ui.fg_dim, bg = colors.palette.inkBg0 },
-          StatusLineNC = { fg = theme.ui.nontext, bg = colors.palette.inkBg0 },
-          MiniStatuslineFilename = { fg = theme.ui.fg_dim, bg = colors.palette.inkBg0 },
-          NormalFloat = { fg = theme.ui.fg, bg = colors.palette.inkBg0 },
-          FloatBorder = { fg = colors.palette.blueSaturated, bg = colors.palette.zenBg0 },
-          FloatTitle = { fg = theme.ui.special, bg = colors.palette.inkBg0, bold = true },
-          DapBreakpoint = { fg = colors.palette.red2Saturated },
-          DapBreakpointLine = { bg = colors.palette.diffRed },
-          DapStopped = { fg = colors.palette.green2Saturated },
-          DapStoppedLine = { bg = colors.palette.diffGreen },
-        }
-      end,
-    },
-    config = function(_, opts)
-      require("kanso").setup(opts)
-      -- vim.cmd.colorscheme("kanso-zen")
-    end,
-  },
-  {
-    "EdenEast/nightfox.nvim",
-    priority = 1000,
   },
 }
