ee9705a0e587fd4314811d54366978b02c03794a
Jean Moggee <jean@mogg.ee>
2025-11-24 07:53:14 +0200

    Updated tabby to be able to toggle time

--- .config/nvim/lua/plugins/tabby.lua
+++ .config/nvim/lua/plugins/tabby.lua
@@ -6,29 +6,37 @@ return {
       current_tab = "TabLineSel",
       tab = "TabLine",
     }
+
+    local show_clock = true
+
+    local function build_tabline(line)
+      local clock = show_clock and { " " .. os.date("%H:%M") .. " ", hl = "Special" } or ""
+      return {
+        line.tabs().foreach(function(tab)
+          local hl = tab.is_current() and theme.current_tab or theme.tab
+          return {
+            " ",
+            tab.number(),
+            ". ",
+            tab.name(),
+            " ",
+            hl = hl,
+          }
+        end),
+        line.spacer(),
+        clock,
+        hl = theme.fill,
+      }
+    end
+
     require("tabby").setup({
-      line = function(line)
-        return {
-          line.tabs().foreach(function(tab)
-            local hl = tab.is_current() and theme.current_tab or theme.tab
-            return {
-              " ",
-              tab.number(),
-              ". ",
-              tab.name(),
-              " ",
-              hl = hl,
-            }
-          end),
-          line.spacer(),
-          {
-            " " .. os.date("%H:%M") .. " ",
-            hl = "Special",
-          },
-          hl = theme.fill,
-        }
-      end,
+      line = build_tabline,
     })
+
+    vim.keymap.set("n", "<leader>w0", function()
+      show_clock = not show_clock
+      vim.cmd("redrawtabline")
+    end, { desc = "Toggle clock" })
 
     vim.keymap.set("n", "<leader>w,", function()
       local snacks = require("snacks")
